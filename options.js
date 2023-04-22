e6b3ebd12ee409173341ed7d0756274b5d33f9f0
lalathealter <musin.daniiil@yandex.ru>
2023-04-22 22:03:00 +0500

    =tweaked and beatifyied the options menu/files a little

--- options.js
+++ options.js
@@ -1,20 +1,44 @@
 
 import { optionsPresetMap, setStoredValue, getStoredValue } from "./store.js"
 
+const elemsThatNeedIndicators = [
+    "cpulimit",
+]
+
 for (const key in optionsPresetMap) {
     defineBehaviour(key)
 }
 
 function defineBehaviour(keyName) {
-    const elem = document.getElementById(keyName)
+    const elem = getSurelyImplemented(keyName)
     elem.value = getStoredValue(keyName)
     elem.onchange = bindSetStoredValue(keyName)
+
+    elem.dispatchEvent(new Event('change')) 
+    // triggering an event to refresh any available indicators
 }
 
 function bindSetStoredValue(valName) {
+    let indicateMethod = function() {}
+    if (elemsThatNeedIndicators.includes(valName)) {
+        indicateMethod = refreshIndicator 
+    } 
     return function(ev) {
         let newVal = ev.target.value
+        indicateMethod(valName, newVal)
         setStoredValue(valName, newVal)
     }
 }
 
+function getSurelyImplemented(elemID) {
+    const htmlElement = document.getElementById(elemID)
+    if (!htmlElement) {
+        alert(`Error! You forgot to implement an html element for the value of ${elemID}`)
+    }
+    return htmlElement
+}
+
+function refreshIndicator(indicatorValName, newVal) {
+    const indicator = getSurelyImplemented(`${indicatorValName}-indicator`)
+    indicator.innerText = `${newVal}%`
+}
